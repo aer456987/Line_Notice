be3162b05478398aca1b110164509002f72525b3
Tori <aer456987@gmail.com>
2020-05-16 14:59:29 +0800

    新增關鍵字&搜尋次數

--- PChome.py
+++ PChome.py
@@ -7,21 +7,46 @@ def getdata(url):
     import json
     datas = json.loads(pc.text)
     datas = datas['prods']   # (字典型態)網頁資料裡的prods字典
+    # print(datas)
     return datas
 
-# 取得商品名稱
-def name(datas):
+# (1)取得商品資訊-名稱/圖片/價格
+def information(datas):
+    lists = []
     for data in datas:
         name = data['name']
-        print(name)
-    return name
+        img = 'https://b.ecimg.tw'+ data['picB']
+        price = data['price']
+        urlid = 'https://24h.pchome.com.tw/prod/' + data['Id']
+        lists.append([name, img, urlid, price])
+    return lists
+
+# 轉換格式
+def content(datas, keyword):
+    for line in datas:
+        name = line[0]
+        if keyword == 'a':
+            print('名稱：', line[0], '\n圖片：', line[1], '\n網址：', line[2], '\n價格：', line[3], '\n')
+        elif  keyword in name:
+            print('名稱：', line[0], '\n圖片：', line[1], '\n網址：', line[2], '\n價格：', line[3], '\n')
+
+# ========================================= #
 
 # 主要函式
 def main():
-    url = 'https://ecshweb.pchome.com.tw/search/v3.3/all/category/DGBJ/results?q=switch&page=1&sort=sale/dc'
-    datas = getdata(url)
-    print(name(datas))
+    count = int(1)
+    keyword = input('輸入關鍵字：')
+    page = input('輸入搜尋次數：')
+    page = int(page) + 1
+    while count<page:
+        url = 'https://ecshweb.pchome.com.tw/search/v3.3/all/category/DGBJ/results?q=SWitch&page=' + str(count) + '&sort=prc/dc'
+        print('【第', count , '筆資料】')
+        count += 1
+        datas = getdata(url)
+        datas = information(datas)
+        print(content(datas, keyword))
+        print('=====================================================')
+
 
 # 呼叫使用
 main()
-
